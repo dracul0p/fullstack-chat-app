c713d5664a42bdba2adb7e60b21dff32551ed767
Asheesh Rawat <asheeshrawat48@gmail.com>
2026-07-29 02:10:48 +0530

    mongodb connection setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,12 @@
 import express from "express";
-const app = express();
-const PORT = process.env.PORT || 5001;
+import dotenv from "dotenv";
+dotenv.config();
 import authRoutes from "./routes/auth.route.js";
+import {connectDB} from "./lib/db.js";
+
+const app = express();
+
+const PORT = process.env.PORT || 5001;
 
 app.get("/", (req, res) => {
   res.send("Backend Running");
@@ -10,5 +15,6 @@ app.get("/", (req, res) => {
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server running on ${PORT}`);
 });
