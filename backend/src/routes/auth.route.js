4057639f16139439fdf4ea0effbd14781f39317a
Asheesh Rawat <asheeshrawat48@gmail.com>
2026-07-29 05:49:59 +0530

    feat: add protectRoute middleware

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -5,6 +5,7 @@ import {
   logout,
   updateProfile,
 } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 const router = express.Router();
 
 // auth
@@ -12,6 +13,7 @@ router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 
-// router.put("/update-profile", protectRoute, updateProfile);
+router.put("/update-profile", protectRoute, updateProfile);
 
 export default router;
+
