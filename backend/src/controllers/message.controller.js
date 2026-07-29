49d765169ecf1bc36ba39aec08ee44adb9fd19e4
Asheesh Rawat <asheeshrawat48@gmail.com>
2026-07-29 07:10:21 +0530

    feat: add get users for sidebar endpoint

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,3 +1,16 @@
+import User from "../models/user.model.js";
+
 export const getUsersForSidebar = async (req, res) => {
-  res.send("users");
+  try {
+    // my self
+    const loggedInUserId = req.user._id;
+    const filteredUsers = await User.find({
+      _id: { $ne: loggedInUserId },
+    }).select("-password");
+
+    res.status(200).json(filteredUsers);
+  } catch (error) {
+    console.error("Error in getUsersForSidebar: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
