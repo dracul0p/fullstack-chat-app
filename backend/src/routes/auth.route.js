cf262567539f13963a8ba04579bc6efe8102bc36
Asheesh Rawat <asheeshrawat48@gmail.com>
2026-07-29 06:58:10 +0530

    auth route completed

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -4,6 +4,7 @@ import {
   login,
   logout,
   updateProfile,
+  checkAuth,
 } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 const router = express.Router();
@@ -14,6 +15,6 @@ router.post("/login", login);
 router.post("/logout", logout);
 
 router.put("/update-profile", protectRoute, updateProfile);
+router.get("/check", protectRoute, checkAuth);
 
 export default router;
-
