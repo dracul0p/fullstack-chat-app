3a90bd06fa79fbcebbec7e0e7addac16ae5d255d
Asheesh Rawat <asheeshrawat48@gmail.com>
2026-07-30 05:38:45 +0530

    basic routes setup in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,23 @@
+import Navbar from "./components/Navbar";
+import HomePage from "./Pages/HomePage";
+import SignUpPage from "./Pages/SignUpPage";
+import LoginPage from "./Pages/LoginPage";
+import SettingsPage from "./Pages/SettingsPage";
+import ProfilePage from "./Pages/ProfilePage";
+
+import { Routes, Route } from "react-router-dom";
 const App = () => {
-  return <div>knkjanlfknkl</div>;
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </div>
+  );
 };
 export default App;
