7d7fa36b62bb2fee25cfc1115b1042cb1e302f07
Asheesh Rawat <asheeshrawat48@gmail.com>
2026-08-03 02:34:39 +0530

    feat: create theme store with Zustand, save theme to localStorage, and build settings page

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
 dotenv.config();
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
+import cors from "cors";
 
 const app = express();
+app.use(cookieParser());
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 
 const PORT = process.env.PORT || 5001;
 
@@ -14,6 +26,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
   connectDB();
