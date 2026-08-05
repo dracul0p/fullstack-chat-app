9e06110ab7b79691d89d06f46943aa6ffa37b1a7
Asheesh Rawat <asheeshrawat48@gmail.com>
2026-08-06 04:40:48 +0530

    created sidebar ui and it featches all users using useChatStore zustang state

---
+++ frontend/src/components/skeletons/SidebarSkeleton.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SidebarSkeleton() {
+  return <div>SidebarSkeleton</div>;
+}
+
+export default SidebarSkeleton;
---
+++ frontend/src/store/useChatStore.js
@@ -0,0 +1,42 @@
+import { create } from "zustand";
+import toast from "react-hot-toast";
+import { axiosInstance } from "../lib/axios";
+import { useAuthStore } from "./useAuthStore";
+
+export const useChatStore = create((set, get) => ({
+  messages: [],
+  users: [],
+  selectedUser: null,
+  isUsersLoading: false,
+  isMessagesLoading: false,
+
+  getUsers: async () => {
+    set({ isUsersLoading: true });
+    try {
+      const res = await axiosInstance.get("/messages/users");
+      set({ users: res.data });
+    } catch (error) {
+      const message = error.response?.data?.message || error.message;
+      console.error("Error in  getUsers while fetching users:", message);
+      toast.error(message);
+    } finally {
+      set({ isUsersLoading: false });
+    }
+  },
+
+  getMessages: async (userId) => {
+    set({ isMessagesLoading: true });
+    try {
+      const res = await axiosInstance.get(`/messages/${userId}`);
+      set({ messages: res.data });
+    } catch (error) {
+      const message = error.response?.data?.message || error.message;
+      console.error("Error in getMessages while fetching messages:", message);
+    } finally {
+      set({ isMessagesLoading: false });
+    }
+  },
+  // todo:optamise this one later
+
+  setSelectedUser: (selectedUser) => set({ selectedUser }),
+}));
