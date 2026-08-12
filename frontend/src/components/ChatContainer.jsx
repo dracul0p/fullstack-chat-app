1d87429a2457e9ff07057c7c6cbe8956473cfe68
Asheesh Rawat <asheeshrawat48@gmail.com>
2026-08-12 23:35:16 +0530

    ChatContainer added

---
+++ frontend/src/components/ChatContainer.jsx
@@ -0,0 +1,32 @@
+import { useChatStore } from "../store/useChatStore";
+import { useEffect } from "react";
+import ChatHeader from "./ChatHeader";
+import MessageInput from "./MessageInput";
+import MessageSkeleton from "./MessageSkeleton";
+const ChatContainer = () => {
+  const { messages, getMessages, isMessagesLoading, selectedUser } =
+    useChatStore();
+
+  useEffect(() => {
+    getMessages(selectedUser.id);
+  }, [selectedUser.id, getMessages]);
+
+  if (isMessagesLoading) {
+    return (
+      <div className="flex-1 flex flex-col overflow-auto">
+        <ChatHeader />
+        <MessageSkeleton />
+        <MessageInput />
+      </div>
+    );
+  }
+  return (
+    <div className="flex-1 flex flex-col overflow-auto">
+      <ChatHeader />
+      <p>messages....</p>
+      <MessageInput />
+    </div>
+  );
+};
+
+export default ChatContainer;
